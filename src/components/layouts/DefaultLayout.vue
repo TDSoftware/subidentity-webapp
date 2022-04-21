20f4f1a9477a77cba0dfb2945d6e0016d408a2e9
sosan-neikbeen <s.neikbeen@tdsoftware.de>
2022-04-21 16:50:07 +0200

    setup default layout

---
+++ src/components/layouts/DefaultLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="subidentity-container">
+    <router-view/>
+  </div>
+  <Footer/>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from "vue-class-component";
+import Footer from "@/components/partials/Footer.vue";
+
+@Options({
+    components: {
+        Footer
+    }
+})
+export default class DefaultLayout extends Vue {
+
+}
+</script>
+<style scoped>
+.subidentity-container {
+  background-image: url("../../assets/logo-blurr.png"), url("../../assets/mainbg.png");
+  background-position: top center;
+  background-repeat: no-repeat, no-repeat;
+  background-size: 700px, cover;
+  height: 93.6vh;
+
+}
+</style>
---
+++ src/components/partials/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-secondary text-white">
+    <div class="container-small">
+      <div class="d-flex justify-content-evenly pt-4 text-white-50">
+        <p class="">© 2022 Web Foundation</p>
+        <span>-</span>
+        <p>Imprint</p>
+        <span>-</span>
+        <p>Disclaimer</p>
+        <span>-</span>
+        <p>Privacy</p>
+        <span>-</span>
+        <p>Cookie</p>
+        <span>-</span>
+        <p>Settings</p>
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from "vue-class-component";
+
+@Options({})
+export default class Footer extends Vue {
+
+}
+</script>
