37a3c9afa71b93c39386a6184c047f4239f2d8aa
sosan-neikbeen <s.neikbeen@tdsoftware.de>
2022-04-25 10:01:35 +0200

    add logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,20 @@
 <template>
-  <div class="home">
-    <!--    <img alt="Vue logo" src="../assets/logo.png"/>-->
-    <!--    <div>-->
-    <!--      <AmazingButton @click="onAmazingButtonClick">-->
-    <!--        Toggle Text-->
-    <!--      </AmazingButton>-->
-    <!--      <p v-if="clicked">-->
-    <!--        Yes that is so beautiful!-->
-    <!--      </p>-->
-    <!--    </div>-->
-    <!--    <p>-->
-    <!--      <AmazingButton class="login-button" @click="toggleAuthenticated">-->
-    <!--        {{ $store.state.isAuthenticated ? "Logout" : "Login" }}-->
-    <!--      </AmazingButton>-->
-    <!--    </p>-->
-  </div>
+    <div class="logo align-items-end h-50">
+        <Logo/>
+    </div>
 </template>
 
 <script lang="ts">
 import {Options, Vue} from "vue-class-component";
-import AmazingButton from "@/components/AmazingButton.vue"; // @ is an alias to /src
+import Logo from "@/components/partials/Logo.vue";
 
 @Options({
     components: {
-        AmazingButton
+        Logo
     }
 })
 export default class HomeView extends Vue {
 
-  private clicked = false;
 
-  onAmazingButtonClick() {
-      console.log("Yeah");
-      this.clicked = !this.clicked;
-  }
-
-    // toggleAuthenticated() {
-    //     if(this.$store.state.isAuthenticated) {
-    //         this.$store.commit("logout");
-    //     } else {
-    //         this.$store.commit("login");
-    //     }
-    // }
 }
 </script>
