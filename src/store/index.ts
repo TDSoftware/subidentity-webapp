cdf30ae3ab5ba031540b9b33968c5c1fb3ca4153
Toni Frotscher <t.frotscher@tdsoftware.de>
2022-04-29 13:03:14 +0200

    storage refactoring
    add isMobileScreen to state

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@ import {createStore, useStore as baseUseStore, Store} from "vuex";
 
 export interface StoreI {
     isAuthenticated: boolean;
+    isMobileScreen: boolean;
 }
 
 export const key: InjectionKey<Store<StoreI>> = Symbol();
@@ -13,15 +14,19 @@ export const useStore = () => {
 
 export const store = createStore({
     state: {
-        isAuthenticated: false
+        isAuthenticated: false,
+        isMobileScreen: false
     },
     getters: {},
     mutations: {
-        login(state: StoreI) {
+        LOGIN(state: StoreI) {
             state.isAuthenticated = true;
         },
-        logout(state: StoreI) {
+        LOGOUT(state: StoreI) {
             state.isAuthenticated = false;
+        },
+        SET_IS_MOBILE_SCREEN(state: StoreI, isMobile: boolean) {
+            state.isMobileScreen = isMobile;
         }
     },
     actions: {
