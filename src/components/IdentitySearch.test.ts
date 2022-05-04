53bcc2258b3eed4564511396fbb065b656cda77d
dobschal <s.dobschal@tdsoftware.de>
2022-05-04 11:22:09 +0200

    remove test

--- src/components/IdentitySearch.test.ts
+++ src/components/IdentitySearch.test.ts
@@ -13,10 +13,6 @@ const mockedSet = mocked(set);
 describe("IdentitySearch.vue", () => {
     describe("when search button is disabled", () => {
 
-        it("should fail...", () => {
-            expect(true).toBe(false);
-        });
-
         it("should not dispatch SEARCH_IDENTITIES action", async () => {
             // mound the component and get the real store instance
             const wrapper = mount(IdentitySearch, {
