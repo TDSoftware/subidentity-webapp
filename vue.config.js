5e8ecd0a3319d0171623b4e8b3b25932499e2d84
dobschal <s.dobschal@tdsoftware.de>
2022-05-05 16:13:45 +0200

    fixbuild

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
     chainWebpack: config => {
+        config.plugins.delete("named-chunks");
         config.module
             .rule("vue")
             .use("vue-loader")
