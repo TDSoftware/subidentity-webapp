88da4caf0bf61d6b3b1e250dc5242784170bc766
sosan-neikbeen <s.neikbeen@tdsoftware.de>
2022-05-17 14:09:22 +0200

    remove in from chain dropdown

--- src/components/partials/IdentitySearch/IdentitySearch.vue
+++ src/components/partials/IdentitySearch/IdentitySearch.vue
@@ -103,16 +103,6 @@ export default class IdentitySearch extends Vue {
         this.selectedChainKey = searchParams.get("chain") ?? "";
         this.loadCustomNodeFromStorage();
         this.setChainOptions();
-
-        //  On page load/reload submit the search if a searchTerm is
-        //  given in the URL params
-        const shouldSubmitSearch =
-            this.searchTerm &&
-            this.selectedChainKey &&
-            this.store.getters.lastSearchTerm !== this.searchTerm;
-        if (shouldSubmitSearch) {
-            this.submitIdentitySearch();
-        }
     }
 
     loadCustomNodeFromStorage() {
@@ -127,7 +117,7 @@ export default class IdentitySearch extends Vue {
         const options = chains.map((chainInfo: ChainInfo) => {
             return {
                 key: chainInfo.key,
-                displayValue: "In " + chainInfo.name
+                displayValue: chainInfo.name
             };
         });
         if (this.customNode) {
