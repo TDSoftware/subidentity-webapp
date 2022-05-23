7c0ba87c1dbca7cadb3fa7499673215dc612cbbd
sosan-neikbeen <s.neikbeen@tdsoftware.de>
2022-05-23 12:56:21 +0200

    update select input when adding new custom node

--- src/components/partials/IdentitySearch/IdentitySearch.vue
+++ src/components/partials/IdentitySearch/IdentitySearch.vue
@@ -177,6 +177,10 @@ export default class IdentitySearch extends Vue {
     onCustomNodeSaved() {
         this.loadCustomNodeFromStorage();
         this.setChainOptions();
+        if (this.customNode) {
+            this.selectedChainKey = this.customNode?.key;
+        }
+
         this.editCustomNodeModalOpen = false;
     }
 }
