62e7ca38e474e0b73c84c9cdc1149562dcf1c49c
MGViereck <m.viereck@tdsoftware.de>
2022-05-30 15:17:20 +0200

    Revert "wrong branch"
    This reverts commit 6e12ea6bce341e1dd321592a0435482c2ea4e5d4.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@ import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
 import {store, key} from "./store";
-import {Toastr} from "./components/partials/profile/plugins/Toastr/Toastr";
 
-createApp(App).use(store, key).use(router).use(Toastr).mount("#app");
+createApp(App).use(store, key).use(router).mount("#app");
