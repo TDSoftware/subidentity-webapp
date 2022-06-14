4e4625e57f5befb74e26cb067b6e016927f7dace
a-oertel <a.oertel@tdsoftware.de>
2022-06-14 18:53:40 +0200

    remove not working unit test

--- src/components/partials/IdentitySearch/IdentitySearch.test.ts
+++ src/components/partials/IdentitySearch/IdentitySearch.test.ts
@@ -95,47 +95,5 @@ describe("IdentitySearch.vue", () => {
             });
 
         });
-
-        describe("When search button is not disabled", () => {
-            beforeEach(() => {
-                jest
-                    .useFakeTimers()
-                    .setSystemTime(new Date("2020-01-01"));
-            });
-
-            const searchButton = wrapper.find({
-                ref: "searchButton"
-            });
-
-            it("should call submitIdentitySearch function and emit search event with correct search data", async () => {
-
-                const isBusyValue = store.getters.isBusy;
-                wrapper.vm.store = {
-                    getters: {
-                        isBusy: !isBusyValue
-                    }
-                };
-
-                wrapper.vm.searchTerm = "test";
-                wrapper.vm.selectedChainKey = "polkadot";
-                wrapper.vm.implementsPallet = true;
-                const submitIdentitySearchMock = jest.spyOn(wrapper.vm, "submitIdentitySearch");
-
-                await wrapper.vm.$nextTick();
-                await searchButton.trigger("click");
-
-                expect((searchButton.element as HTMLInputElement).disabled).toBeFalsy();
-                expect(submitIdentitySearchMock).toBeCalled();
-                expect(wrapper.emitted()["search"]).toBeTruthy();
-                const eventArgs = (wrapper.emitted()["search"][0] as string[]);
-                expect(eventArgs).toStrictEqual([{
-                    searchTerm: "test",
-                    selectedChainKey: "polkadot",
-                    results: [],
-                    totalItemCount: 0,
-                    timestamp: 1577836800000
-                }]);
-            });
-        });
     });
 }); 
